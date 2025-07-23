50096b020c8c9440208d07953194143658924a60
Geunwon Park <rmsdnjsaos@gmail.com>
2025-07-24 00:59:54 +0900

    #1 test(domain.performance/review): 공연, 후기 도메인 테스트 코드 리뷰 반영
    - 두 번째 코드 리뷰에 관하여, 반영하였습니다.
    
    Resolves: #1
    See also: None

--- src/pages/performance/detail/PerformanceReviewForm.tsx
+++ src/pages/performance/detail/PerformanceReviewForm.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 
 interface ReviewFormProps {
 	onSubmit: (content: string) => void;
@@ -7,7 +7,8 @@ interface ReviewFormProps {
 export default function ReviewForm({ onSubmit }: ReviewFormProps) {
 	const [content, setContent] = useState("");
 
-	const handleSubmit = () => {
+	const handleSubmit = (e: FormEvent) => {
+		e.preventDefault();
 		if (!content.trim()) return;
 
 		onSubmit(content);
@@ -17,17 +18,14 @@ export default function ReviewForm({ onSubmit }: ReviewFormProps) {
 	const disabledButton = content.trim().length === 0;
 
 	return (
-		<div>
+		<form onSubmit={handleSubmit}>
 			<textarea
 				value={content}
 				onChange={(e) => setContent(e.target.value)}
 				placeholder="후기를 작성해주세요."
 				rows={4}
 			/>
-			<button
-				type="button"
-				onClick={handleSubmit}
-				disabled={disabledButton}>{`>`}</button>
-		</div>
+			<button type="submit" disabled={disabledButton}>{`>`}</button>
+		</form>
 	);
 }
